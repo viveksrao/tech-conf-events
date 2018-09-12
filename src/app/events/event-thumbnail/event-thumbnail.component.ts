518e7f7d4cd1b0a3f295ba75c6d7ceb38bda74b8
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-11 19:29:17 -0700

    Updated @Input variable named 'event' to 'techConfEvent'.

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EventThumbnailComponent implements OnInit {
 
-  @Input() event: any;
+  @Input() techConfEvent: any;
 
   constructor() { }
 
