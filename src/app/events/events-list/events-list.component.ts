3db44c94ce5222009284cb5c0b7400f6c49f7075
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-11 21:57:35 -0700

    Implemented 'handleThumbnailClick(techConfEventName)' and made use of success method from ToastrService.

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TechConfEventsDataService } from '../shared/tech-conf-events-data.service';
+import { ToastrService } from '../../common/toastr.service';
 
 @Component({
   selector: 'app-events-list',
@@ -9,10 +10,14 @@ import { TechConfEventsDataService } from '../shared/tech-conf-events-data.servi
 export class EventsListComponent implements OnInit {
   techConfEventsData:any[];
 
-  constructor(private techConfEventsDataService: TechConfEventsDataService) { }
+  constructor(private techConfEventsDataService: TechConfEventsDataService, private toastrService: ToastrService) { }
 
   ngOnInit() {
     this.techConfEventsData = this.techConfEventsDataService.getTechConfEvents();
+  }
+
+  handleThumbnailClick(techConfEventName){
+    this.toastrService.success(techConfEventName);
   }
 
 }
