075d59d27606d739f8d725e20aa098fa2165205c
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-12 22:37:49 -0700

    Created AuthService.

---
+++ src/app/user/shared/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  currentUser: IUser;
+
+  constructor() { }
+
+  loginUser(userName: string, password: string){
+    this.currentUser = {
+      id: 1,
+      userName: userName,
+      firstName: 'Tom',
+      lastName: 'Papa'
+    }
+  }
+
+  isAuthenticated(){
+    return !!this.currentUser;
+  }
+}
