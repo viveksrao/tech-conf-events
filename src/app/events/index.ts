23f4ea87f081bfabc8cfc09c99f296d08e5878c6
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-13 15:46:08 -0700

    Added CreateSessionComponent.

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -1,4 +1,5 @@
 export * from './create-event/create-event.component';
+export * from './create-session/create-session.component';
 export * from './event-details/event-details.component';
 export * from './event-thumbnail/event-thumbnail.component';
 export * from './events-list/events-list.component';
