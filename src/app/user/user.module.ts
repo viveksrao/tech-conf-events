949984a9485464269b758c3ed9e4f5b9a793fd2a
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-12 22:18:15 -0700

    Added LoginComponent in declarations array and FormsModule in imports array.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { userRoutes } from './user.routes';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ]
 })
 export class UserModule { }
