81fcb1c98cda9aeb106db641aaa51d5db44d4b67
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-13 19:58:10 -0700

    Added './restricted-words.validator' to the barrel.

--- src/app/events/shared/index.ts
+++ src/app/events/shared/index.ts
@@ -2,3 +2,4 @@ export * from './tech-conf-events-data.service';
 export * from './events-list-resolver.service';
 export * from './event-route-activator.service';
 export * from './event.model';
+export * from './restricted-words.validator';
