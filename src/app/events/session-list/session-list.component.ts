ec142ffcca0f29aa444c7ef60834340c4f13c548
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-14 12:46:15 -0700

    Implemented Sorting of Session Data.

--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-session-list',
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
 
   @Input() sessions:ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(){
+    if(this.sessions){
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter){
+    if(filter === 'all'){
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
   }
 
 }
