d0030f145b6002881e27f5a88d146218f007568c
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-13 19:57:38 -0700

    Added restrictedWords Validator to 'abstract' field.

--- src/app/events/create-session/create-session.component.ts
+++ src/app/events/create-session/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
-import { ISession } from '../shared';
+import { ISession, restrictedWords } from '../shared';
 
 @Component({
   selector: 'app-create-session',
@@ -23,7 +23,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
     this.newSessionForm = new FormGroup({
       name: this.name,
