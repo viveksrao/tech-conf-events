f44e3e308a573c12a57d27f7320e48c5cae280fe
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-16 15:33:27 -0700

    Using @Input Setters.

--- src/app/events/upvote/upvote.component.ts
+++ src/app/events/upvote/upvote.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class UpvoteComponent implements OnInit {
 
   @Input() count: number;
-  @Input() voted: boolean;
+  iconColor: string;
+  @Input() set voted(val){
+    this.iconColor = val ? 'red' : 'white';
+  };
   @Output() vote = new EventEmitter();
+  
 
   constructor() { }
 
