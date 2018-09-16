817cb80820bdd4314d10a8e72074716fd903b18f
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-16 09:39:14 -0700

    Using the @ViewChild Decorator.

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './index';
 
 @Component({
   selector: 'app-simple-modal',
@@ -9,10 +10,15 @@ export class SimpleModalComponent implements OnInit {
 
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalcontainer') containerEl: ElementRef;
 
-  constructor() { }
+  constructor(@Inject(JQ_TOKEN) private $: any) { }
 
   ngOnInit() {
+  }
+
+  closeModal(){
+    this.$(this.containerEl.nativeElement).modal('hide');
   }
 
 }
