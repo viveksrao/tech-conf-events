3ed6890c06278c0f112d8d048e496973666bf090
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-16 09:50:53 -0700

    Creating Settings on Components – Created closeOnBodyClick setting on SimpleModalComponent.

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -11,6 +11,7 @@ export class SimpleModalComponent implements OnInit {
   @Input() title: string;
   @Input() elementId: string;
   @ViewChild('modalcontainer') containerEl: ElementRef;
+  @Input() closeOnBodyClick: string;
 
   constructor(@Inject(JQ_TOKEN) private $: any) { }
 
@@ -18,7 +19,9 @@ export class SimpleModalComponent implements OnInit {
   }
 
   closeModal(){
-    this.$(this.containerEl.nativeElement).modal('hide');
+    if(this.closeOnBodyClick.toLocaleLowerCase() === "true"){
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
   }
 
 }
