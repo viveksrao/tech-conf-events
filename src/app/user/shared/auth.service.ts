83932207c1757e1667ef0f29068963dbb1d99325
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-17 16:19:11 -0700

    Saving User Data to the Server – Updated updateCurrentUser method.

--- src/app/user/shared/auth.service.ts
+++ src/app/user/shared/auth.service.ts
@@ -48,5 +48,7 @@ export class AuthService {
   updateCurrentUser(firstName: string, lastName: string){
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
+    let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+    return this.http.put(`/api/users/${this.currentUser.id}`, this.currentUser, options);
   }
 }
