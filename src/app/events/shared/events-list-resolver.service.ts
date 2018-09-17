baf0378634d9396d8f162a066f45cf5b0404cc28
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-16 21:51:53 -0700

    Removed not in use map import.

--- src/app/events/shared/events-list-resolver.service.ts
+++ src/app/events/shared/events-list-resolver.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { TechConfEventsDataService } from './tech-conf-events-data.service';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
