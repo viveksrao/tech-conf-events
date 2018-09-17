eab8b4ba2fcf05ea27b4b541c91da5eb7e0557d7
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-17 16:03:14 -0700

    Persisting Authentication Status Across Page – Calling  checkAuthenticationStatus method from ngOnInit method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './user/shared/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.authService.checkAuthenticationStatus();
+  }
 }
