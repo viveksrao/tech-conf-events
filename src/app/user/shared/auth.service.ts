260495492624fcf9a5684758f40a14ab037bd695
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-17 15:29:30 -0700

    Integrated Authentication with the Server – updated loginUser method.

--- src/app/user/shared/auth.service.ts
+++ src/app/user/shared/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +11,23 @@ export class AuthService {
 
   currentUser: IUser;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   loginUser(userName: string, password: string){
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: 'Tom',
-      lastName: 'Papa'
-    }
+    let loginInfo = {
+      username: userName,
+      password: password
+    };
+    let options = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    return this.http.post('/api/login', loginInfo, options)
+        .pipe(tap(data => {
+          this.currentUser = <IUser>data['user']
+        }))
+        .pipe(catchError(err => {
+          return of(false)
+        }));
   }
 
   isAuthenticated(){
