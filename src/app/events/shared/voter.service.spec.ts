5a305e36d3261de067364b46e1181a6ba5328840
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-18 14:58:13 -0700

    Unit Testing Voter Service.

--- src/app/events/shared/voter.service.spec.ts
+++ src/app/events/shared/voter.service.spec.ts
@@ -1,12 +1,27 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { VoterService } from './voter.service';
+import { ISession } from '../shared/event.model';
+import { of } from 'rxjs';
 
 describe('VoterService', () => {
+  let voterService: VoterService,
+      mockHttp;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [VoterService]
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete','post'])
+    voterService = new VoterService(mockHttp);
+  });
+
+  describe('deleteVoter', () => {
+    
+    it('should remove the voter from the list of voters', () => {
+      var session = { id:6, voters: ["joe", "john"] };
+      mockHttp.delete.and.returnValue(of(false));
+      
+      voterService.deleteVoter(3, <ISession>session, "joe");
+      
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe("john");
     });
   });
-  
+
 });
