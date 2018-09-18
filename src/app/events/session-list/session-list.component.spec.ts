1e41277a524d17d6eb80f9fd1a2b05c329e4ad1a
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-18 16:14:21 -0700

    Testing Components with Isolated Tests.

--- src/app/events/session-list/session-list.component.spec.ts
+++ src/app/events/session-list/session-list.component.spec.ts
@@ -1,22 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SessionListComponent } from './session-list.component';
+import { ISession } from '../shared/event.model';
 
 describe('SessionListComponent', () => {
   let component: SessionListComponent;
-  let fixture: ComponentFixture<SessionListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SessionListComponent ]
-    })
-    .compileComponents();
-  }));
+  let mockAuthService, mockVoterService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SessionListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new SessionListComponent(mockAuthService, mockAuthService);
   });
 
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'Session 1', level: 'intermediate'},
+        { name: 'Session 2', level: 'advanced'},
+        { name: 'Session 3', level: 'intermediate'}];
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.techConfEventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'Session 1', level: 'intermediate'},
+        { name: 'Session 3', level: 'advanced'},
+        { name: 'Session 2', level: 'intermediate'}];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.techConfEventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions[2].name).toBe('Session 3');
+    });
+  });
 });
