a94cf83ec3e7e2f16144e655e804766c7db56332
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-09-18 17:41:56 -0700

    Testing Components with Deep Integrated Tests.

---
+++ src/app/events/session-list/session-list.component.integrated.spec.ts
@@ -0,0 +1,58 @@
+import {TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser'
+
+import { SessionListComponent } from './session-list.component';
+import { AuthService } from '../../user/shared/auth.service';
+import { VoterService } from '../shared/voter.service';
+import { ISession } from '../shared/event.model';
+import { UpvoteComponent } from '../upvote/upvote.component';
+import { DurationPipe } from '../shared/duration.pipe';
+import { CollapsibleWellComponent } from '../../common/collapsible-well.component';
+
+describe('SessionListComponent', () => {
+  let fixture: ComponentFixture<SessionListComponent>,
+      component: SessionListComponent,
+      element: HTMLElement,
+      debugEl: DebugElement;
+  
+  beforeEach(async(() => {
+    let mockAuthService = {};
+    let mockVoterService = {};
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [
+        SessionListComponent, 
+        UpvoteComponent, 
+        CollapsibleWellComponent
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: VoterService, useValue: mockVoterService }
+      ],
+      schemas:[]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
+  });
+
+  describe('initial display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [{ id: 3, name: 'Session 1', presenter: 'Joe', duration: 1, level: 'beginner', abstract: 'abstract', voters: ['john', 'joe']}];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.techConfEventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+    });
+  });
+});
